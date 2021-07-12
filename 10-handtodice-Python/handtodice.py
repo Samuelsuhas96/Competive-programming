e9df10cf7100c8248e09ca66ae056f9ce41b724e
Samuelsuhas96 <bellapusamuelsuhas@msitprogram.net>
2021-07-12 21:15:27 +0530

    2020501003_handtodice

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -6,5 +6,37 @@
 # Hint: You might find // and % useful here, and also getKthDigit().
 
 def handtodice(hand):
-	# your code goes here
-	pass
+	# a = hand%10 # for the last digit/dice
+	# r = hand//10
+	# b = r%10 # for the second digit/dice
+	# c = r//10 # for the first digit/dice
+	r = []
+	# c = len(str(hand))
+	# hand = str(hand)
+	# while(len(hand) >=0):
+	# 	hand = int(hand)
+	# 	# print(hand)
+	# 	# to retrieve the digits as they are.
+	# 	t = hand // 10**(c-1)
+	# 	# print(t)
+	# 	r.append(t)
+	# 	if(hand == 0):
+	# 		break
+	# 	hand = hand % 10**(c-1)
+	# 	hand = str(hand)
+	# 	# print(hand)
+	# r.append(0)
+	# return tuple(r)
+	while(hand != 0):
+		a = hand % 10
+		r.append(a)
+		hand = hand // 10
+	return tuple(r[::-1])
+
+print(handtodice(422))
+		
+
+
+
+	
+
