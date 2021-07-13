c1fa65e1757b80eb45dd1c8e3adcfe7ce2329c0d
Samuelsuhas96 <bellapusamuelsuhas@msitprogram.net>
2021-07-13 12:30:14 +0530

    2020501003_setkthdigit

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,44 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+		# print(n,k,d)
+		s="" 
+		s1=""
+		s2 =""
+		negative = False
+		if n<0:
+			s = str(abs(n))
+			negative = True
+		else:
+			s = str(n)
+		if len(s) > k:
+			s1 = s[::-1]
+			# print(s1[k])
+			v = s1[k]
+			v1 = str(d)
+			a = s1.replace(v,v1)
+			# print(a)
+			s2 = a[::-1]
+			# print(s2)
+			if negative == True:
+				return -(int(s2))
+			else:
+				return int(s2)
+		elif (len(s) <= k):
+			s3 = "0"*(k-(len(s)-1)) + s
+			# print(s3)
+			s1 = s3[::-1]
+			# print(s1[k])
+			v = s1[k]
+			v1 = str(d)
+			a = s1.replace(v,v1)
+			# print(a)
+			s2 = a[::-1]
+			# print(s2)
+			if negative == True:
+				return -(int(s2))
+			else:
+				return int(s2)
+
+print(fun_set_kth_digit(-168,3,1))
 
