caf82764877d655164f35c7789ce7f04ecf432e1
Samuelsuhas96 <bellapusamuelsuhas@msitprogram.net>
2021-08-02 15:23:59 +0530

    2020501003_nthsmithnumber

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,68 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+def sum_of_digits(n):
+    sum = 0
+    while(n>0):
+        a = n%10
+        sum = sum + a
+        n = n//10
+    return sum
+
+def composite_check(n):
+    count = 2
+    if n == 2:
+        return False
+    elif n == 1 or n % 2 == 0:
+        return True
+    else:
+        for i in range(3,n,2):
+            if n%i == 0:
+                count += 1
+        if count > 2:
+            return True
+        else:
+            return False
+
+def prime_check(n):
+    count = 2
+    if n == 2:
+        return True
+    elif n == 1 or n % 2 == 0:
+        return False
+    else:
+        for i in range(3,n,2):
+            if n%i == 0:
+                count += 1
+        if count > 2:
+            return False
+        else:
+            return True
 
 def fun_nth_smithnumber(n):
-    return 1
+    count = 0
+    i = 2
+    prime_factor_sum = 0
+    while(True):
+        # print(i)
+        if composite_check(i):
+            print(i)
+            for j in range(2,i):
+                if i%j == 0:
+                    if prime_check(j):
+                        print("primefactor: ",j)
+                        prime_factor_sum+=sum_of_digits(j)
+                        # print(prime_factor_sum)
+            print(i,prime_factor_sum,sum_of_digits(i))
+            if sum_of_digits(i) == prime_factor_sum:
+                count += 1
+                if count == n:
+                    return i
+            # print("yes")
+        prime_factor_sum = 0
+        i = i + 1
+
+# print(sum_of_digits(123))
+# print(composite_check(12))
+# print(prime_check(12))
+print(fun_nth_smithnumber(2))
